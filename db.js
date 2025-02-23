f2c3a383d4d664d052d34b45748b56e6688282fb
Giulia Moukouyou <giuliamoukouyou@gmail.com>
2025-02-23 11:55:54 +0100

    Add .env, .gitignore, db.js and test-db.js file.

---
+++ db.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+// @ts-ignore
+const sql = require("mssql");
+
+// Configurazione della connessione
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  port: parseInt(process.env.DB_PORT, 10),
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+  },
+};
+
+// Funzione per connettersi al database
+async function connectDB() {
+  try {
+    await sql.connect(config);
+    console.log("✅ Connesso a SQL Server!");
+  } catch (error) {
+    console.error("❌ Errore nella connessione al database:", error);
+  }
+}
+
+module.exports = { connectDB, sql };
+
+
---
+++ test/test-db.js
@@ -0,0 +1,3 @@
+const {connectDB, sql} = require("../db");
+
+connectDB();
